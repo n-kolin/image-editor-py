43b9483a28a2e601353f715c3894354499d433ae
n-kolin <1020nomi@gmail.com>
2025-11-30 23:40:48 +0200

    res.partts

--- app.py
+++ app.py
@@ -91,24 +91,25 @@ def gemini_gen_image():
     user_prompt = request_data.get('prompt', 'Hello, Gemini!')
     logger.info(f"Received Gemini prompt: {user_prompt}")
     
+
     try:
-        model = genai.GenerativeModel('gemini-2.5-flash')
-        response = model.generate_content('Please summarise this document: ...')
+        model = genai.GenerativeModel('gemini-2.5-flash-image')
+        response = model.generate_content("Create a picture of a nano banana dish in a fancy restaurant with a Gemini theme")
 
         # response = client.models.generate_content(
         #     model="gemini-2.5-flash-image", contents=user_prompt
         # )
         logger.info("Gemini response received successfully", response)
     #     for part in response.parts:
-    # if part.text is not None:
-    #     print(part.text)
-    # elif part.inline_data is not None:
-    #     image = part.as_image()
-    #     image.save("generated_image.png")
+    #       if part.text is not None:
+    #           print(part.text)
+    #       elif part.inline_data is not None:
+    #           image = part.as_image()
+    #           image.save("generated_image.png")
         return jsonify({
             "status": "success",
             "data": {
-                "response_parts": response.text
+                "response_parts": response.parts
             }
         })
         
